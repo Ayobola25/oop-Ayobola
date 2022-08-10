30931e3c8d4d6fc57c6684b81097ebc16090b435
Support <Support@WEMA-TRAINING-S>
2022-08-10 12:52:28 -0700

    Add project files.

---
+++ oop Ayobola/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_Ayobola
+{
+    internal interface IBakery 
+    {
+        public double Price(double amount);
+    }
+
+    public class BakeryOrders : IBakery
+    {
+
+        public BakeryOrders()
+        {
+            PriceSmallBread = 100;
+            PriceMediumBread = 300;
+            PriceLargeBread = 500;
+        }
+
+        public double PriceSmallBread { get; private set; }
+        public double PriceMediumBread { get; private set; }
+        public double PriceLargeBread { get; private set; }
+
+        public double Price(double smallAmount)
+        { 
+           double price = PriceSmallBread * smallAmount; 
+            return price;
+        }
+
+        public double Price(double smallAmount, double mediumAmount)
+        {
+            double price = (PriceSmallBread * smallAmount) + (PriceMediumBread * mediumAmount);
+            return price;
+        }
+            
+            
+        public double Price(double smallAmount, double mediumAmount, double largeAmount) 
+        {
+               double price = (PriceSmallBread * smallAmount) + (PriceMediumBread * mediumAmount) + (PriceLargeBread * largeAmount);
+                return (price);
+        }
+        
+    }
+
+}
+
+
+    
+
---
+++ oop Ayobola/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace oop_Ayobola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my Bakery App");
+
+            var bakeryOrders = new BakeryOrders();
+            Console.WriteLine("\nThe price for our Small Bread is "+ bakeryOrders.PriceSmallBread + " Naira");
+            Console.WriteLine("\nThe price for our Medium Bread is "+ bakeryOrders.PriceMediumBread + " Naira");
+            Console.WriteLine("\nThe price for our Large Bread is "+ bakeryOrders.PriceLargeBread + " Naira");
+
+            Console.WriteLine("\nwhat type of bread do you want to buy? for small bread, press S, for medium bread press M, for large bread press L") ;
+            var breadType = Console.ReadLine();
+            Console.WriteLine("how many bread do you want to buy? ");
+            var breadNumber = Convert.ToDouble(Console.ReadLine());
+
+            if (breadType.ToUpper() == "S")
+            {
+                var price = bakeryOrders.Price(breadNumber);
+                Console.WriteLine("Your amount due is " + price);
+            }
+            else if (breadType.ToUpper() == "M")
+            {
+                var price = bakeryOrders.Price(0, breadNumber);
+                Console.WriteLine("Your amount due is " + price);
+
+
+            }
+            else if (breadType.ToUpper() == "L")
+            {
+                var price = bakeryOrders.Price(0,0,breadNumber);
+                Console.WriteLine("Your amount due is " + price);
+
+
+            }
+
+
+
+        }
+    }
+}
